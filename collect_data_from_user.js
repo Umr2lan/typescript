379915f26a5782982efee111d55bc03c5d13c26e
HasnainSiddiqui11 <aptech7ut@gmail.com>
2025-02-19 18:54:44 +0500

    group adding

--- collect_data_from_user.js
+++ collect_data_from_user.js
@@ -5,5 +5,21 @@ function collect_data_from_user() {
     var nearest_landmark = document.getElementById("nearest_landmark").value;
     var owner_name = document.getElementById("owner_name").value;
     var owner_age = document.getElementById("owner_age").value;
-    console.log("Shop Name: ".concat(shop_name, "\nTiming: ").concat(timing, "\nAddress: ").concat(address, "\nNearest Landmark: ").concat(nearest_landmark, "\nOwner Name: ").concat(owner_name, "\nOwner Age: ").concat(owner_age));
+    var category = document.getElementById("category").value;
+    var mall_name = document.getElementById("mall_name").value;
+    var gender = document.getElementsByName("gender");
+    var yesorno = document.getElementsByName("yesorno");
+    var gender_value = "";
+    var yesorno_value = "";
+    for (var i = 0; i < gender.length; i++) {
+        if (gender[i].checked) {
+            gender_value = gender[i].value;
+        }
+    }
+    for (var o = 0; o < yesorno.length; o++) {
+        if (yesorno[o].checked) {
+            yesorno_value = yesorno[o].value;
+        }
+    }
+    console.log("Shop Name is: ".concat(shop_name, "\nTiming is: ").concat(timing, "\nAddress is: ").concat(address, "\nNearest Landmark is: ").concat(nearest_landmark, "\nOwner Name is: ").concat(owner_name, "\nOwner Age is: ").concat(owner_age, "\nCategory is: ").concat(category, "\nMall Name is: ").concat(mall_name, "\nGender is: ").concat(gender_value, "\nYes or No is: ").concat(yesorno_value));
 }
--- collect_data_from_user.ts
+++ collect_data_from_user.ts
@@ -5,8 +5,27 @@ function collect_data_from_user(){
     let nearest_landmark = (document.getElementById("nearest_landmark") as HTMLInputElement).value;
     let owner_name = (document.getElementById("owner_name") as HTMLInputElement).value;
     let owner_age = (document.getElementById("owner_age") as HTMLInputElement).value;
+    let category = (document.getElementById("category") as HTMLSelectElement).value;
+    let mall_name = (document.getElementById("mall_name") as HTMLSelectElement).value;
+    let gender = document.getElementsByName("gender") as NodeListOf<HTMLInputElement>;
+    let yesorno = document.getElementsByName("yesorno") as NodeListOf<HTMLInputElement>;
+    
+    let gender_value = "";
+    let yesorno_value = "";
 
-    console.log(`Shop Name is: ${shop_name}\nTiming is: ${timing}\nAddress is: ${address}\nNearest Landmark is: ${nearest_landmark}\nOwner Name is: ${owner_name}\nOwner Age is: ${owner_age}`);
+
+    for(let i = 0; i < gender.length; i++){
+        if((gender[i] as HTMLInputElement).checked){
+            gender_value = (gender[i] as HTMLInputElement).value;
+        }
+    }
+    for(let o = 0; o < yesorno.length; o++){
+        if((yesorno[o] as HTMLInputElement).checked){
+            yesorno_value = (yesorno[o] as HTMLInputElement).value;
+        }
+    }
+
+    console.log(`Shop Name is: ${shop_name}\nTiming is: ${timing}\nAddress is: ${address}\nNearest Landmark is: ${nearest_landmark}\nOwner Name is: ${owner_name}\nOwner Age is: ${owner_age}\nCategory is: ${category}\nMall Name is: ${mall_name}\nGender is: ${gender_value}\nYes or No is: ${yesorno_value}`);
 }
 
 
